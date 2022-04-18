b29748746502b7c13c8301aae83ab3f4c4778858
Saviour <1907saviour@gmail.com>
2022-04-18 10:51:29 +0530

    addded subject field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -128,19 +128,21 @@ const links = [
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    
       <form
       name="contact-form"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
+      
     >
       <input name="name" placeholder="Your Name" type="text" />
       <input name="email" placeholder="name@name.com" type="email" />
       <textarea name="message" />
+      <input type='hidden' name='subject' value='New form enquiry'/>
       <button>Send</button>
     </form>
-    </main>
+    
     // <main style={pageStyles}>
     //   <title>Home Page</title>
     //   <h1 style={headingStyles}>
